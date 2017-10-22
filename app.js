8402cf005e5a5edb0c151679c9eb2e3a256e7885
Stewart <stew.m.craig@gmail.com>
2017-10-22 19:36:20 +1100

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 8080;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/:date', (req, res) => {
+  const inputDate = req.params.date;
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  let naturalDate = null;
+  let resDate = {};
+  // Convert date to integer if date is a number
+  // required for parsing unix date to Date constructor
+  const date = isNaN(inputDate) ? new Date(inputDate) : new Date(parseInt(inputDate, 10));
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  naturalDate = date.getTime() ?
+    `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
+    : null;
+
+  resDate = {
+    unix: date.getTime(),
+    natural: naturalDate,
+  };
+
+  res.end(JSON.stringify(resDate));
+});
+
+app.listen(port);
